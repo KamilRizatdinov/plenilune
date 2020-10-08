e2a50c5b3852e40d8f7e1a2550084130a85ee040
kamil <kamil@kamils-MacBook-Pro.local>
2020-10-08 18:04:36 +0300

    Fix bug of init storage API request

--- client/client.py
+++ client/client.py
@@ -96,13 +96,13 @@ def initialize(block_size=1024):
     '''
     response = requests.get(f'http://{name_server_address}/init',
                             {"blocksize": block_size})
-                            
+
     data = response.json()
     storage_server_addresses = data["servers"]
     
     response = requests.post(
             f'http://{storage_server_addresses[0]}/init',
-            json={'servers': storage_server_addresses}
+            json=storage_server_addresses
     )
     print('System initialized')
 
