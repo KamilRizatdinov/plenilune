ce2d316b3de0c8c5e2b18ca9fac6d498ba1fc25b
kamil <kamil@kamils-MacBook-Pro.local>
2020-10-09 00:33:08 +0300

    Fix typo

--- name_server/dataworker.py
+++ name_server/dataworker.py
@@ -48,7 +48,7 @@ def get_active_storage_servers_hostnames():
 
 
 def get_random_active_storage_server():
-    storage_servers = get_data()["stoage_servers"]
+    storage_servers = get_data()["storage_servers"]
     active_storage_servers = [storage_server for storage_server in storage_servers if storage_server["status"] == "UP"]
     storage_server = random.choice(active_storage_servers)
     return storage_server
